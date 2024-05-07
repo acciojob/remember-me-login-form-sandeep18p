6b7aeaacb8f83d83aef19cef755f56a26911daa1
Sandeep Pansari <86551476+sandeep18p@users.noreply.github.com>
2024-05-07 19:55:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
-  function saveToLocalStorage(key, value) {
+ function saveToLocalStorage(key, value) {
             localStorage.setItem(key, value);
         }
 
-        // Function to remove data from local storage
         function removeFromLocalStorage(key) {
             localStorage.removeItem(key);
         }
 
-        // Function to check if data exists in local storage
         function checkLocalStorage(key) {
             return localStorage.getItem(key) !== null;
         }
 
-        // Function to retrieve data from local storage
         function getFromLocalStorage(key) {
             return localStorage.getItem(key);
         }
 
         document.getElementById('submit').addEventListener('click', function(event) {
             event.preventDefault();
-			var rememberCheckbox = document.getElementById('checkbox');
+            var rememberCheckbox = document.getElementById('checkbox');
 
             if (rememberCheckbox.checked) {
                 var username = document.getElementById('username').value;
@@ -34,7 +31,7 @@
 
             var loggedInUser = getFromLocalStorage('username');
             if (loggedInUser) {
-                alert('Logged in as username' + loggedInUser);
+                alert('Logged in as ' + loggedInUser);
                 var existingButton = document.createElement('button');
                 existingButton.id = 'existing';
                 existingButton.textContent = 'Login as existing user';
