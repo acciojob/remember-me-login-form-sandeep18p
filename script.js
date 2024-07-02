6e9dfafb8dd54e63d1f4214a994a235b710bb297
Sandeep Pansari <86551476+sandeep18p@users.noreply.github.com>
2024-07-02 18:11:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,43 +1,35 @@
- function saveToLocalStorage(key, value) {
-            localStorage.setItem(key, value);
-        }
+function saveToLocalStorage(key, value) {
+    localStorage.setItem(key, value);
+}
 
-        function removeFromLocalStorage(key) {
-            localStorage.removeItem(key);
-        }
+function removeFromLocalStorage(key) {
+    localStorage.removeItem(key);
+}
 
-        function checkLocalStorage(key) {
-            return localStorage.getItem(key) !== null;
-        }
+function checkLocalStorage(key) {
+    return localStorage.getItem(key) !== null;
+}
 
-        function getFromLocalStorage(key) {
-            return localStorage.getItem(key);
-        }
+function getFromLocalStorage(key) {
+    return localStorage.getItem(key);
+}
 
-        document.getElementById('submit').addEventListener('click', function(event) {
-            event.preventDefault();
-            var rememberCheckbox = document.getElementById('checkbox');
+document.getElementById('submit').addEventListener('click', function(event) {
+    event.preventDefault();
+    var rememberCheckbox = document.getElementById('checkbox');
 
-            if (rememberCheckbox.checked) {
-                var username = document.getElementById('username').value;
-                var password = document.getElementById('password').value;
+    if (rememberCheckbox.checked) {
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
 
-                saveToLocalStorage('username', username);
-                saveToLocalStorage('password', password);
-            } else { 
-                removeFromLocalStorage('username');
-                removeFromLocalStorage('password');
-            }
-
-            var loggedInUser = getFromLocalStorage('username');
-            if (loggedInUser) {
-                alert('Logged in as ' + loggedInUser);
-                var existingButton = document.getElementById('existing');
-				existingButton.style.display='block';
-                
-
-                existingButton.addEventListener('click', function() {
-                    alert('Logged in as ' + loggedInUser);
-                });
-            }
-        });
+        saveToLocalStorage('username', username);
+        saveToLocalStorage('password', password);
+    } else { 
+        removeFromLocalStorage('username');
+        removeFromLocalStorage('password');
+    }
+    var loggedInUser = getFromLocalStorage('username');
+    alert('Logged in as ' + loggedInUser);
+    var loggedInUser = getFromLocalStorage('username');
+  
+});
